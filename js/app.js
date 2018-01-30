f7627cddb810ebf052d1d1f0c2bd5e4cb5503eb6
HirakN <hiraknaik.hn@gmail.com>
2018-01-30 23:05:37 +0000

    added template file and created paddle and balls

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// Get canvas element
+var canvas = document.getElementById("myCanvas");
+// Drawing object
+var ctx = canvas.getContext("2d");
+
+createPaddles()
+
+createBall();
+
+function createPaddles() {
+	h = canvas.height;
+	w = canvas.width;
+	padHeight = 100;
+	padWidth = 10;
+
+	ctx.fillStyle = "white";
+	ctx.fillRect(0,h/3,padWidth,padHeight);
+	ctx.fillRect(w - 10,h/3,padWidth,padHeight);
+}
+
+function createBall() {
+	ctx.fillStyle = "white";
+	ctx.arc(250,150,5,0,2*Math.PI);
+	ctx.fill();
+}
